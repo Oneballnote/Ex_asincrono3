af480718338146025154e7f660acad529c39202f
Yaotecatl Martinez <oneballnotita@gmail.com>
2023-03-31 17:42:27 -0600

    Punto 1 completo y testeato

---
+++ Arboles.java
@@ -0,0 +1,31 @@
+package Ex_asincrono3;
+
+/**
+ * Arboles
+ */
+public class Arboles {
+    /*
+    public boolean existsOddSumPath(Node root, int x) {
+        if (root == null) {
+            return false;
+        }
+    
+        return existsOddSumPathHelper(root, 0, x);
+    }
+    
+    private boolean existsOddSumPathHelper(Node node, int sum, int x) {
+        if (node == null) {
+            return false;
+        }
+    
+        sum += node.value % 2 == 1 ? 1 : 0;
+        
+        if (node.left == null && node.right == null) {
+            return sum == x;
+        }
+    
+        return existsOddSumPathHelper(node.left, sum, x) || existsOddSumPathHelper(node.right, sum, x);
+    }
+    
+*/
+}
